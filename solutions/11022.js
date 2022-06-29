36aa4efa42cc0a0fdb15c49889547a577a16f420
Dongbin Na / 나동빈 <ndb796@naver.com>
2022-06-29 20:47:09 +0900

    Update 11022.js

--- solutions/11022.js
+++ solutions/11022.js
@@ -12,3 +12,11 @@ for (let t = 1; t <= testCase; t++) {
   let b = Number(data[1]);
   console.log(`Case #${t}: ${a} + ${b} = ${a + b}`); // 템플릿 리터럴을 활용한 문자열 출력
 }
+
+/*
+let n = Number(input[0]);
+for (let i = 1; i <= n; i++) {
+  let [a, b] = input[i].split(' ').map(Number);
+  console.log(`Case #${i}: ${a} + ${b} = ${a + b}`);
+}
+*/
